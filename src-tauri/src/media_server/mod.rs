65bdee68a095f951939dda8b985565210edbd54a
h8m0ss <danarmenta32@gmail.com>
2025-06-03 13:31:28 -0600

    FIX: Removed fields that we don't know if we'll use

--- src-tauri/src/media_server/mod.rs
+++ src-tauri/src/media_server/mod.rs
@@ -4,16 +4,12 @@ use std::collections::hash_map::HashMap;
 
 pub struct MediaServer {
   workspace: String,
-  media: Vec<MediaItem>,
-  tag_index: HashMap<String, Vec<u32>>
 }
 
 impl MediaServer {
   pub fn new(workspace: String)->MediaServer {
     MediaServer { 
       workspace: workspace, 
-      media: Vec::new(), 
-      tag_index: HashMap::new() 
     }
   }
 }
